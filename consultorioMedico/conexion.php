1ae8d73b3934384604a8c903cfaf4483c01df23d
Fabian Cordoba <fabiansb98@gmail.com>
2022-07-18 11:36:05 -0600

    manejo de errores en conexion sql

--- consultorioMedico/conexion.php
+++ consultorioMedico/conexion.php
@@ -1,4 +1,6 @@
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "consultoriomedico") #conexion a BD
-    #falta manejo de errores
+    $mysqli = new mysqli("localhost", "root", "", "consultoriomedico"); #conexion a BD
+    if ($mysqli->connect_error) {
+        die("Error al Conectar con la Base de Datos: " . $mysqli->connect_error);
+    } # manejo de errores
 ?>
