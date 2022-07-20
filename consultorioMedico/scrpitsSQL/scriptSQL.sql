1ed3753accf37bdcc0caeb16081ec8a6b0d09fcf
Fabian Cordoba <fabiansb98@gmail.com>
2022-07-20 14:46:43 -0600

    Agregando INSERTS

--- consultorioMedico/scrpitsSQL/scriptSQL.sql
+++ consultorioMedico/scrpitsSQL/scriptSQL.sql
@@ -42,6 +42,10 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
 
+INSERT INTO `consultoriomedico`.`doctor` (idDoctor,nombreDoctor,cedulaDoctor,telefonoDoctor,correoDoctor,especialidad,tipoUsuario) VALUES (1,'Doctor1',12345678,22221111,'correoD2@gmail.com','Pediatra',2);
+INSERT INTO `consultoriomedico`.`doctor` (idDoctor,nombreDoctor,cedulaDoctor,telefonoDoctor,correoDoctor,especialidad,tipoUsuario) VALUES (2,'Doctor2',12345679,11112222,'correoD2@gmail.com','Cirujano',2);
+
+
 CREATE TABLE `consultoriomedico`.`cita` (
   idCita INT NOT NULL AUTO_INCREMENT,
   fechaCita DATE NOT NULL,
@@ -52,6 +56,10 @@ CREATE TABLE `consultoriomedico`.`cita` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
+
+INSERT INTO `consultoriomedico`.`cita` (idCita,fechaCita,descripcion,doctorAsignado) VALUES (1,'2022-07-20','Consulta General',1);
+INSERT INTO `consultoriomedico`.`cita` (idCita,fechaCita,descripcion,doctorAsignado) VALUES (2,'2022-07-20','Consulta General',2);
+
 
 CREATE TABLE `consultoriomedico`.`pacientes` (
   idPaciente INT NOT NULL AUTO_INCREMENT,
@@ -66,3 +74,6 @@ CREATE TABLE `consultoriomedico`.`pacientes` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish_ci;
+
+INSERT INTO `consultoriomedico`.`pacientes` (idPaciente,nombrePaciente,cedulaPaciente,fechaNacimiento,telefonoPaciente,correoPaciente,citaPaciente) VALUES (1,'Paciente1',44443333,"1998-11-20",99998888,'paciente1@gmail.com',1);
+INSERT INTO `consultoriomedico`.`pacientes` (idPaciente,nombrePaciente,cedulaPaciente,fechaNacimiento,telefonoPaciente,correoPaciente,citaPaciente) VALUES (2,'Paciente2',33334444,"1990-04-20",88889999,'paciente2@gmail.com',2);
