bf729ea3602bb38f377d5dc19e362b82f0c75d7a
Fabian Cordoba <fabiansb98@gmail.com>
2022-08-27 18:09:59 -0600

    Cambios finales

--- consultorioMedico/menus/menuEmpleados.php
+++ consultorioMedico/menus/menuEmpleados.php
@@ -65,11 +65,7 @@
                             </a>
                             <div class="collapse" id="collapseEmpleado" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
-<<<<<<< HEAD
                                     <a class="nav-link" href="./formularios/registrarEmpleado.php">Agregar Empleado</a>
-=======
-                                    <a class="nav-link" href="./formularios/registrarEmpleado.php">Agregar Doctor</a>
->>>>>>> 6a40a144e1f5d6c7feba682f6026b5817f2d1715
                                 </nav>
                             </div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseCitas" aria-expanded="false" aria-controls="collapseLayouts">
@@ -153,36 +149,18 @@
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Pacientes</div>
+                                    <div class="card-body">Registrar Empleados</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="./menus/menuPacientes.php">View Details</a>
+                                        <a class="small text-white stretched-link" href="../formularios/registrarEmpleados.php">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Empleado</div>
+                                    <div class="card-body">Buscar Empleado</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Citas</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="./menus/menuCitas.php">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Usuarios</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="./menus/menuUsuarios.php">View Details</a>
+                                        <a class="small text-white stretched-link" href="../tablas/tablaEmpleado.php">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
